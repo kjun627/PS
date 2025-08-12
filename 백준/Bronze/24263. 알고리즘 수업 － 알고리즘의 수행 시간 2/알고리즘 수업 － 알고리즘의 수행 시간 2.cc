56da902c3eaee02bf77ed244af9c4e7e58864721
Kim Jun Hyung <145478388+kjun627@users.noreply.github.com>
2025-08-12 12:19:16 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+
+int main(){
+        int num;
+        cin >> num;
+
+        cout << num << endl;
+        cout << 1 << endl;
+
+        return 0;
+}
